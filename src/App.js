54953570a32a6f4244addcfd959cab31f8326a7d
Maduro29 <manhdungtran.vnuuet@gmail.com>
2023-04-23 23:57:50 +0700

    JSX commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
+    <div className="container">
+      <div className="row">
+        <div className="todolist">
+          <h1>Todos</h1>
+          <ul className="list-unstyled">
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value=""/>MAD Lions
+                </label>
+              </div>
+            </li>
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value=""/>GAM
+                </label>
+              </div>
+            </li>
+            <li className="ui-state-default">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" value=""/>KT Rolster
+                </label>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
