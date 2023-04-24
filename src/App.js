544fbc8e252deaa783ec223c07736943e2e8f3f8
Maduro29 <manhdungtran.vnuuet@gmail.com>
2023-04-24 16:48:22 +0700

    Render-data commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
 import './App.css';
 
 function App() {
+  let items = [
+    {
+      text: 'GAMTIME',
+      status: false
+    },
+    {
+      text: 'T1',
+      status: true
+    },
+    {
+      text: 'KT',
+      status: false
+    }
+  ];
+
+  let title = 'things to do';
+
   return (
     <div className="container">
       <div className="row">
         <div className="todolist">
-          <h1>Todos</h1>
+          <h1>{title.toUpperCase()}</h1>
           <ul className="list-unstyled">
-            <li className="ui-state-default">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" value=""/>MAD Lions
-                </label>
-              </div>
-            </li>
-            <li className="ui-state-default">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" value=""/>GAM
-                </label>
-              </div>
-            </li>
-            <li className="ui-state-default">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" value=""/>KT Rolster
-                </label>
-              </div>
-            </li>
+            {items.map(item => (
+              <li className="ui-state-default">
+                <div className="checkbox">
+                  <label>
+                    <input type="checkbox" value=""/>{item.text}
+                  </label>
+                </div>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
