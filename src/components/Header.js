62ba8bd59c0ba4ffb1af5081497d64f90697c88d
Maduro29 <manhdungtran.vnuuet@gmail.com>
2023-04-25 00:53:32 +0700

    input-component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Input from "./Input";
 
 const Header = (props) => {
     let data = props;
 
     return (
-        <h1>{data.title.toUpperCase()}</h1>
+        <header>
+            <h1>{data.title.toUpperCase()}</h1>
+            <Input />
+        </header>
     );
 }
 
---
+++ src/components/Input.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Input = () => {
+    return (
+        <input type="text" className="form-control add-todo" placeholder="Add new thing..."/>
+    );
+}
+
+export default Input
