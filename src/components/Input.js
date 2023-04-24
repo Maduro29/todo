d0dafa94f322ef67a66c7805f4426ad04475647f
Maduro29 <manhdungtran.vnuuet@gmail.com>
2023-04-25 01:34:16 +0700

    set State for input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,9 +1,26 @@
 import React from "react";
 
-const Input = () => {
-    return (
-        <input type="text" className="form-control add-todo" placeholder="Add new thing..."/>
-    );
+// const Input = () => {
+//     return (
+//         <input type="text" className="form-control add-todo" placeholder="Add new thing..."/>
+//     );
+// }
+
+class Input extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value || ' '
+        };
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value});
+    }
+
+    render() {
+        return <input type="text" className="form-control add-todo" value={this.state.value} onChange={this.handleChange.bind(this)} placeholder="Add new thing..."/>
+    }
 }
 
 export default Input
